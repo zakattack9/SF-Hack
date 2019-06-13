f1f313acec866e0de53ea64fcc6fa17894be7813
gndeshpa <gauravd257@gmail.com>
2019-06-13 03:50:08 -0700

    Fix messages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HomeMain from './components/HomeMain';
 import HomeNotifications from './components/HomeNotifications';
 import ReportedOutagesRow from './components/ReportedOutagesRow';
+import WeatherUpdateRows from './components/WeatherUpdateRows';
 
 function App() {
   return (
     <div className="App">
       {/* <HomeMain/>
       <HomeNotifications/> */}
-      <ReportedOutagesRow/>
+      {/* <ReportedOutagesRow/> */}
+      <WeatherUpdateRows/>
     </div>
   );  
 }
--- src/components/ReportedOutagesRow.js
+++ src/components/ReportedOutagesRow.js
@@ -8,10 +8,10 @@ function ReportedOutagesRow(props) {
         <div>  
              <SearchBar  width = "20%" />
             <h1 className = 'mainTitle'>Reported Outages</h1>
-            <ReportedOutagesNotifications location = 'Tempe, AZ' time = '8:03 am' stateFarmLocation = 'Marina Heights' description = "All systems and servers have gone down due to a mass power outage cause by heavy rain and wind in the Tempe area"/>
-            <ReportedOutagesNotifications location = 'Phoenix, AZ' time = '8:03 am' stateFarmLocation = 'Main Headquarters' description = "A part of claims department is down due to a power outage that affected the south side of the Bloomington headquarters"/>
-            <ReportedOutagesNotifications location = 'Phoenix, AZ' time = '8:03 am' stateFarmLocation = 'Marina Heights' description = "All systems and servers have gone down due to a power outage"/>
-            <ReportedOutagesNotifications location = 'Phoenix, AZ' time = '8:03 am' stateFarmLocation = 'Marina Heights' description = "All systems and servers have gone down due to a power outage"/>
+            <ReportedOutagesNotifications location = 'Tempe, AZ' time = '10:03 am' stateFarmLocation = 'Marina Heights' description = "All systems and servers have gone down due to a mass power outage cause by heavy rain and wind in the Tempe area"/>
+            <ReportedOutagesNotifications location = 'Bloomington, IL' time = '8:03 am' stateFarmLocation = 'Main Headquarters' description = "A part of claims department is down due to a power outage that affected the south side of the Bloomington headquarters"/>
+            <ReportedOutagesNotifications location = 'Atlanta, GA' time = '8:03 am' stateFarmLocation = 'Atlanta Hub' description = "The large majority of Atlanta systems and servers have gone down due to a power outage"/>
+            <ReportedOutagesNotifications location = 'Dallas, TX' time = '8:03 am' stateFarmLocation = 'Dallas Hub' description = "Enterprise Technology at the Dallas Hub is experiencing communication issues due to a power outage"/>
         </div>
     )
 }
