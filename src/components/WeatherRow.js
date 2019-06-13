8e872f91818b640fad90d0e41b21b14a2fd69558
Zak Sakata <zakattack26111@gmail.com>
2019-06-12 22:31:49 -0700

    stashing changes

--- src/components/WeatherRow.js
+++ src/components/WeatherRow.js
@@ -5,13 +5,13 @@ import WeatherNotifications from './WeatherNotifications'
 
 function WeatherRow(props) {
 
-    return (
-        <div>   
-             <h1 className = 'mainTitle'>Weather <br/> Updates</h1>
-            <WeatherNotifications location = 'Phoenix, AZ' time = '8:03 pm' weatherTitle = 'Heavy Flooding' description = "Accuweather has reported heavy rain for the area of Phoenix"/>
-            <WeatherNotifications location = 'Bloomington, IL' time = '5:30 am' weatherTitle = 'Strong Winds' description = "Accuweather has reported heavy rain for the area of Phoenix"/>
-        </div>
-    )
-} 
+  return (
+    <div>
+      <h1 className='mainTitle'>Weather <br /> Updates</h1>
+      <WeatherNotifications location='Phoenix, AZ' time='8:03 pm' weatherTitle='Heavy Flooding' description="Accuweather has reported heavy rain for the area of Phoenix" />
+      <WeatherNotifications location='Bloomington, IL' time='5:30 am' weatherTitle='Strong Winds' description="Accuweather has reported heavy rain for the area of Phoenix" />
+    </div>
+  )
+}
 
 export default WeatherRow;
