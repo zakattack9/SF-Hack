1bb3d7feeb76963df87c34e1ac0ca01b5c339958
vman08 <vikram8dattatri@gmail.com>
2019-06-13 07:10:53 -0700

    Outage Layout Fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import HomeMain from './components/HomeMain';
 import HomeNotifications from './components/HomeNotifications';
-// import ReportedOutagesRow from './components/ReportedOutagesRow';
+import ReportedOutagesRow from './components/ReportedOutagesRow';
 import LocationStats from './components/LocationStats';
 
 class App extends React.Component {
