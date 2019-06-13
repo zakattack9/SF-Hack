3615215bcc1ab6d098a040e09d5a3596c7fb100e
Zak Sakata <zakattack26111@gmail.com>
2019-06-12 23:48:30 -0700

    added outage row

--- src/components/OutageRow.js
+++ src/components/OutageRow.js
@@ -1,23 +1,24 @@
-import React from 'react';  
+import React from 'react';
 import './OutageRow.css';
 import UserOutageNotification from './UserOutageNotification';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
-function OutageRow(props) {
-
-    return (
-
-        <div >
-            
-            <h1 className = 'title' >Reported <br/> Outages</h1>
-            <UserOutageNotification  time = '2:05 pm' office = 'Marina Heights'location = 'Phoenix, AZ' />
-            <UserOutageNotification  time = '6:03 am' office = 'Statefarm Texas' location = 'Dallas, TX'/>
-            <UserOutageNotification  time = '5:45 am' office = 'Atlanta Hub' location = 'Atlanta, GA'/>
+const OutageRow = props => {
+  return (
+    <div className="OutageRow">
+      <div className="titleWrapper">
+        <h1 className='mainTitle'>Reported<br />Outages</h1>
+        <div className="boltIcon">
+          <FontAwesomeIcon icon={faBolt} size="lg" />
         </div>
-
-
-
-    )
-
+      </div>
+      <UserOutageNotification time='2:05 pm' office='Marina Heights' location='Phoenix, AZ' />
+      <UserOutageNotification time='6:03 am' office='Statefarm Texas' location='Dallas, TX' />
+      <UserOutageNotification time='5:45 am' office='Atlanta Hub' location='Atlanta, GA' />
+      <div className="viewAll">View All</div>
+    </div>
+  )
 }
 
 export default OutageRow;
